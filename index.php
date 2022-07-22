af8a9de3671d2238be8ae14e78d71675d27b840d
arie <arieinjector@gmail.com>
2022-07-22 07:53:46 +0700

    move to php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gambar JO</title>
+</head>
+<body>
+    <p>Halo</p>
+
+    <form action="/action.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="imageupload">
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
